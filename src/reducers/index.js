b1cd0f253a36f9798cf51386f2c0657a9507cdb5
quentin <quentin.mberi@gmail.com>
2020-04-14 10:15:46 +0200

    Ajout du code

---
+++ src/reducers/index.js
@@ -0,0 +1,20 @@
+const initialState = {
+    articles: [],
+    remoteArticles: [],
+};
+
+function rootReducer(state = initialState, action) {
+    switch (action.type) {
+        case 'ADD_ARTICLE':
+            return Object.assign({}, state, {
+                articles: state.articles.concat(action.payload)
+            });
+        case 'DATA_LOADED':
+            return Object.assign({}, state, {
+                remoteArticles: state.remoteArticles.concat(action.payload)
+            });
+        default:
+            return state;
+    }
+}
+export default rootReducer;
